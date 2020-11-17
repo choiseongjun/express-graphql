da3dbcaca26c278321b59e39c2fc22e64c1e8010
최성준 <csj2702@naver.com>
2020-11-17 16:28:28 +0900

    graphql세팅 및 더미데이터로 조회

---
+++ schema/schema.js
@@ -0,0 +1,81 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType
+        , GraphQLString
+        , GraphQLSchema
+        , GraphQLID
+        , GraphQLInt
+         } = graphql;
+
+// dummy data
+const customer = [
+    { id:1,code:"c01",name:"choiseongjun",mobile:"01030022702"}
+];
+const order = [
+    {id:1,code:"o01",customer_id:"1",detail:{model:"iPhone X",color:"Space Gray",size:"large",order_date:"2020년 10월"},status:"delivered"}
+]
+const CustomerType = new GraphQLObjectType({
+    name: 'customer',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        code: { type: GraphQLString },
+        name: { type: GraphQLString },
+        mobile:{type:GraphQLString}
+    })
+});
+const OrderType = new GraphQLObjectType({
+    name: 'Order',
+    fields: ( ) => ({
+        id: { type: GraphQLID },
+        code: { type: GraphQLString },
+        status:{type:GraphQLString},
+        detail:{type:detailType},
+        customer_id:{type:GraphQLInt},
+        customer:{
+            type:CustomerType,
+            resolve(parent,args){
+                console.log(parent)
+                return _.find(customer,{id:parent.id});
+            }
+        }
+    })
+});
+const detailType = new graphql.GraphQLObjectType({
+    name: 'Author_detail',
+    fields: {
+      model: { type: graphql.GraphQLString },
+      color: { type: graphql.GraphQLString },
+      size: { type: graphql.GraphQLString },
+      order_date:{type:graphql.GraphQLString}
+    }
+  });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: { id: { type: GraphQLInt } },
+            resolve(parent, args){
+                // code to get data from db / other source
+                console.log(parent)
+                return _.find(customer, { id: args.id });
+            }
+        },
+        order:{
+            type:OrderType,
+            args:{id:{type:GraphQLInt}},
+            resolve(parent, args){
+                // code to get data from db / other source
+                console.log(parent)
+                return _.find(order, { id: args.id });
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
